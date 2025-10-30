8ee7c380ddcec026ebbf75c1e61c18695e6f40d8
noheliasanchez111 <noheliasanchez111@gmail.com>
2025-10-30 06:34:25 +0000

    Actualización en el ejercicio

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -14,11 +14,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const CalculadoraFactorial(title: 'Flutter Demo Home Page'),
+      home: const Calculator(title: 'Calculadora de Factorial'),
     );
   }
 }
 
-class CalculadoraFactorial extends StatefulWidget {
-    @override
-  const CalculadoraFactorial({super.key, required this.title});
+  class Calculator extends StatefulWidget {
+  final String title;
+  const Calculator({super.key, required this.title});
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final TextEditingController _controller = TextEditingController();
+  String? _result;
+  bool _isLoading = false;
+
+  void _calculateFactorial() async {
+    final input = int.tryParse(_controller.text);
+    if (input == null || input < 0) {
+      setState(() => _result = 'Por favor ingresa un número válido');
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+      _result = null;
+    });
+
+    final value = await _recursiveFactorial(input);
+
+    setState(() {
+      _isLoading = false;
+      _result = 'Factorial de $input es $value';
+    });
+  }
+
+  Future<int> _recursiveFactorial(int n) async {
+    return n <= 1 ? 1 : n * await _recursiveFactorial(n - 1);
+  }
